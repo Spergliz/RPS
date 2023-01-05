e84e2ec572e1f1f32917cb6c53369ff5382da678
liz <97650879+Spergliz@users.noreply.github.com>
2023-01-05 14:18:28 -0700

    wfa

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("rock").addEventListener("click", RPS)
+document.getElementById("paper").addEventListener("click", RPS)
+document.getElementById("scissors").addEventListener("click", RPS)
+let outputE1 = document.getElementById("output1")
+let outputE2 = document.getElementById("output2")
+
+function RPS(){
+    
+}
