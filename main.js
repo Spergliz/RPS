3a380b1d33381466452a423b5593953963475af7
liz <97650879+Spergliz@users.noreply.github.com>
2023-01-05 14:51:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,14 @@ let outputE1 = document.getElementById("output1")
 let outputE2 = document.getElementById("output2")
 
 function RPS(){
-    
+    let CPU = Math.random();
+    let player = 
+if(CPU<0.3) {
+ CPU = 'Rock'} else 
+ if( CPU<0.6) {
+    CPU= 'Paper'
+ }else {
+    CPU='Scissors'
+}
+ let winner = checkwinner 
 }
